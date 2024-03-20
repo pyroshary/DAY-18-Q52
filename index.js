0cca259b3d16ba1ada7f962cc6969f29c6d348a8
pyroshary <sheharyarghulamrasool@gmail.com>
2024-03-21 02:13:11 +0500

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//making smart phone detials
+var smartphone = { make: "APPLE",
+    year: 2022,
+    model: 12,
+    specs: { ram: "8gb", rom: "256gb", screensize: "12.4inchs" }
+};
+console.log(smartphone);
---
+++ index.ts
@@ -0,0 +1,10 @@
+//making smart phone detials
+
+let smartphone = 
+{make:"APPLE",
+year: 2022,
+model: 12,
+specs:{ram:"8gb",rom:"256gb",screensize:"12.4inchs"}
+}
+
+console.log(smartphone);
